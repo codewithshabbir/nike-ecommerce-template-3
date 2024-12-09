355dbd25ebf804dff9e50de64f149342e66531c7
Muhammad Shabbir <110288732+muhammadshabbir07@users.noreply.github.com>
2024-12-09 23:28:10 +0500

    ui card issue fixed

--- src/app/components/Cards/Card.tsx
+++ src/app/components/Cards/Card.tsx
@@ -23,7 +23,7 @@ const Card: React.FC<Product> = ({
 }) => {
   return (
     <div className="mb-6 md:mb-6 m-auto md:m-0 px-2 focus-visible:outline-none">
-      <Link href={`/all-products/${id}`}
+      <Link className="focus-visible:outline-none" href={`/all-products/${id}`}
       >
         <Image className="focus-visible:outline-none" src={imagesUrls} alt={title} width={300} height={300} />
       <div className="py-4 focus-visible:outline-none">
