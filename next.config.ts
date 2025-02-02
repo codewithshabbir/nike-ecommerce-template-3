155d2c3b2dbdb5c9d389b925027db1a8d511fd69
Muhammad Shabbir <110288732+muhammadshabbir07@users.noreply.github.com>
2025-02-02 15:15:01 +0500

    api fetching problem fixed

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,22 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  /* Other config options */
-
   images: {
-    domains: ['nike-ecommerce-template-3.vercel.app', 'cdn.sanity.io'],  // Allow loading images from this domain
+    domains: ['nike-ecommerce-template-3.vercel.app', 'cdn.sanity.io'],
   },
 
   webpack: (config) => {
     config.resolve.alias["@public"] = path.resolve(__dirname, "public");
     return config;
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/countries",
+        destination: "https://nike-ecommerce-template-3.vercel.app/api/countries",
+      },
+    ];
   },
 };
 
